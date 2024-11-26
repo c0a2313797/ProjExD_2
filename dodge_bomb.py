6da483eaa4336a4ab5a24e4c4cb395377565a1b7
C0A23137 <c0a2313797@edu.teu.ac.jp>
2024-11-26 14:27:04 +0900

    爆弾生成ランダム移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -21,7 +22,12 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    #bb_img = pg.draw.circle(bg_img,(255,0,0),(10,10),10)
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_img.set_colorkey((0,0,0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    vx,vy = 5,5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -36,16 +42,14 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += delta[0]
                 sum_mv[1] += delta[1]
-        # if key_lst[pg.K_UP]:
-        #     sum_mv[1] -= 5
-        # if key_lst[pg.K_DOWN]:
-        #     sum_mv[1] += 5
-        # if key_lst[pg.K_LEFT]:
-        #     sum_mv[0] -= 5
-        # if key_lst[pg.K_RIGHT]:
-        #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        bb_rct.move_ip(vx,vy)
+        # if bb_rct.left < 0 or bb_rct.right > WIDTH:
+        #     vx = -vx
+        # if bb_rct.top < 0 or bb_rct.bottom > WIDTH:
+        #     vy = -vy
         screen.blit(kk_img, kk_rct)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
